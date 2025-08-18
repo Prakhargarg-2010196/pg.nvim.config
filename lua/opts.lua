c2ce37882d72ca60bc1b77b73553f32d6745eb09
Prakhar <23prakhargarg2002@gmail.com>
2025-08-18 11:54:23 +0530

    feat(base):Added base configuration for nvim

---
+++ lua/opts.lua
@@ -0,0 +1,23 @@
+-- vim.g.mapleader = " "
+vim.o.nu = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 3
+vim.o.softtabstop = 3
+-- vim.o.shiftwidth = 3
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+vim.o.wrap = false
+
+-- vim.o.hlsearch = false
+-- vim.o.incsearch = true
+
+vim.o.swapfile = false
+vim.o.backup = false
+-- vim.opt.undofile = true
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- vim.opt.scrolloff = 8
+-- vim.o.updatetime = 50
+-- vim.opt.rocks.hererocks= false
